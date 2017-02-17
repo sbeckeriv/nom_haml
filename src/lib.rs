de69d80fd213a5b05c84f1f63ab43e1083f235b4
Stephen Becker IV <github@deathbyescalator.com>
2017-02-16 22:18:40 -0800

    was missing classes
    Shocked it was this easy to add. shocked! whitespace was harder to
    figure out.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,22 @@
 #[macro_use]
 extern crate nom;
-use nom::{IResult,digit, space, alpha, alphanumeric};
-use std::str::{self,FromStr};
+use nom::{IResult, digit, space, alpha, alphanumeric};
+use std::str::{self, FromStr};
 use std::collections::HashMap;
 #[derive(Debug)]
-struct HAMLParser{
+struct HAMLParser {
     haml: String,
 }
 
 type AttrMap = HashMap<String, String>;
 
-#[derive(Debug, PartialEq)]
-struct Node{
+#[derive(Debug, Clone, PartialEq)]
+struct Node {
     children: Vec<Node>,
     tag: String,
     attributes: Option<AttrMap>,
     id: Option<String>,
+    class: Vec<String>,
 }
 
 
@@ -32,6 +33,14 @@ named!(double_quote_string<&str>,
     tag!("\""),
     map_res!(escaped!(call!(alphanumeric), '\\', is_a!("\"n\\")), std::str::from_utf8),
     tag!("\"")
+  )
+);
+
+named!(tag_class<&str>,
+  chain!(
+    tag!(".") ~
+    class: map_res!( alphanumeric, std::str::from_utf8),
+    || class
   )
 );
 
@@ -89,68 +98,141 @@ named!(attributes_list<AttrMap>,
     )
 );
 
+named!(html_line<(Vec<&str>, Node)>,
+       do_parse!(
+               whitespace: many0!(map_res!(alt!(tag!(" ")|tag!("\t")), str::from_utf8)) >>
+               node: html_tag >>
+               (whitespace, node)
+               )
+       );
+
 named!(html_tag<(Node)>,
        do_parse!(
                tag: opt!(complete!(tag_named)) >>
                id: opt!(complete!(tag_id)) >>
+               class: many0!(tag_class) >>
                attributes_list: opt!(complete!(attributes_list)) >>
-               (Node{children: vec![], tag: String::from(tag.unwrap_or("div")), id: id.map(|text| String::from(text)), attributes: attributes_list }))
+               (Node{children: vec![], tag: String::from(tag.unwrap_or("div")),
+                   id: id.map(|text| String::from(text)),
+                   attributes: attributes_list,
+                   class: class.into_iter().map(|text| String::from(text)).collect(),
+               })
+               )
        );
 
 #[cfg(test)]
 mod tests {
-    use nom::{IResult};
+    use nom::IResult;
     use super::*;
 
     #[test]
     fn it_parses_tag_id() {
         let empty = &b""[..];
-        //should not allow this kind of set.
+        // should not allow this kind of set.
         assert_eq!(tag_id("#id".as_bytes()), IResult::Done(empty, ("id")));
     }
 
     #[test]
     fn it_parses_tag_nammed() {
         let empty = &b""[..];
-        //should not allow this kind of set.
+        // should not allow this kind of set.
         assert_eq!(tag_named("%p".as_bytes()), IResult::Done(empty, ("p")));
+    }
+
+    #[test]
+    fn it_parses_html_line() {
+        let empty = &b""[..];
+        // should not allow this kind of set.
+        let mut attrs = AttrMap::new();
+
+        let node = Node {
+            class: vec![],
+            children: vec![],
+            tag: "p".to_string(),
+            id: None,
+            attributes: None,
+        };
+        assert_eq!(html_line("%p".as_bytes()), IResult::Done(empty, (vec![], node.clone())));
+        assert_eq!(html_line("\t\t%p".as_bytes()), IResult::Done(empty, (vec!["\t","\t"], node.clone())));
+        assert_eq!(html_line("  %p".as_bytes()), IResult::Done(empty, (vec![" "," "], node)));
     }
 
     #[test]
     fn it_parses_html_tag() {
         let empty = &b""[..];
-        //should not allow this kind of set.
-        let mut attrs= AttrMap::new();
-        let node =  Node{children: vec![], tag: "p".to_string(), id: None, attributes: None};
+        // should not allow this kind of set.
+        let mut attrs = AttrMap::new();
+
+        let node = Node {
+            class: vec![],
+            children: vec![],
+            tag: "p".to_string(),
+            id: None,
+            attributes: None,
+        };
         assert_eq!(html_tag("%p".as_bytes()), IResult::Done(empty, (node)));
-        let node =  Node{children: vec![], tag: "p".to_string(), id: Some("banana".to_string()), attributes: None};
+
+        let node = Node {
+            class: vec![],
+            children: vec![],
+            tag: "p".to_string(),
+            id: Some("banana".to_string()),
+            attributes: None,
+        };
         assert_eq!(html_tag("%p#banana".as_bytes()), IResult::Done(empty, (node)));
-        attrs.insert("d".to_string(),"3".to_string());
-        let node =  Node{children: vec![], tag: "p".to_string(), id: Some("banana".to_string()), attributes: Some(attrs.clone())};
+
+        let node = Node {
+            class: vec!["pan".to_string(),"cakes".to_string()],
+            children: vec![],
+            tag: "p".to_string(),
+            id: Some("banana".to_string()),
+            attributes: None,
+        };
+        assert_eq!(html_tag("%p#banana.pan.cakes".as_bytes()), IResult::Done(empty, (node)));
+
+        attrs.insert("d".to_string(), "3".to_string());
+        let node = Node {
+            class: vec![],
+            children: vec![],
+            tag: "p".to_string(),
+            id: Some("banana".to_string()),
+            attributes: Some(attrs.clone()),
+        };
         assert_eq!(html_tag("%p#banana(:d=>3)".as_bytes()), IResult::Done(empty, (node)));
-        let node =  Node{children: vec![], tag: "p".to_string(), id: None, attributes: Some(attrs.clone())};
+
+        let node = Node {
+            class: vec![],
+            children: vec![],
+            tag: "p".to_string(),
+            id: None,
+            attributes: Some(attrs.clone()),
+        };
         assert_eq!(html_tag("%p(:d=>3)".as_bytes()), IResult::Done(empty, (node)));
     }
 
     #[test]
     fn it_parses_attributes_not_allowed() {
         let empty = &b""[..];
-        let mut attrs= AttrMap::new();
-        attrs.insert("a".to_string(),"3".to_string());
-        //should not allow this kind of set.
+        let mut attrs = AttrMap::new();
+        attrs.insert("a".to_string(), "3".to_string());
+        // should not allow this kind of set.
         assert_eq!(attributes_list("(:a => 3}".as_bytes()), IResult::Done(empty, attrs));
     }
 
     #[test]
     fn it_parses_attributes() {
         let empty = &b""[..];
-        let mut attrs=AttrMap::new();
+        let mut attrs = AttrMap::new();
         assert_eq!(attributes_list("{}".as_bytes()), IResult::Done(empty,attrs.clone()));
-        attrs.insert("a".to_string(),"3".to_string());
+
+        attrs.insert("a".to_string(), "3".to_string());
         assert_eq!(attributes_list("(:a=>3)".as_bytes()), IResult::Done(empty, attrs.clone()));
+
         assert_eq!(attributes_list("{:a=>\"3\"}".as_bytes()), IResult::Done(empty, attrs.clone()));
+
         assert_eq!(attributes_list("{:a=> '3'}".as_bytes()), IResult::Done(empty, attrs.clone()));
-        attrs.insert("b".to_string(),"4".to_string());
+
+        attrs.insert("b".to_string(), "4".to_string());
         assert_eq!(attributes_list("{:a=>3, :b=>'4'}".as_bytes()), IResult::Done(empty, attrs));
     }
 }
